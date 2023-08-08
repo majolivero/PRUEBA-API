6365fb308bd0cc0449748c40c3a0fdccb4fd9d5c
Julian Munoz Bedoya <jmunoz@spear-tech.com>
2023-08-07 19:11:57 -0500

    cambios hechos para que la petición funcione

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
-let request = require("request");
 const port = 3000;
 const path = require("path");
+const https = require('https');
 
 /*function obtenerPersonas() {
   const datos_ejemplo = {
@@ -24,8 +24,22 @@ const path = require("path");
   });
 }*/
 
+
 app.get("/",(request, response) => {
   response.sendFile(path.resolve(__dirname, 'index.html'));
+})
+
+app.get("/obtener/:amount",(request, response) => {
+  https.get(`https://8j5baasof2.execute-api.us-west-2.amazonaws.com/production/tests/trucode/samples?size=${request.params.amount}`, (res) => {
+    let data = '';
+    res.on('data', (chunk) =>{
+      data += chunk;
+    });
+
+    res.on('end', () => {
+      response.send(data.split('\n'));
+    });
+  });
 })
 
 app.listen(port,() => {
