bb087021e3d25ac7a4eade2fa8a849703b7b3a0a
Julian Munoz Bedoya <jmunoz@spear-tech.com>
2023-08-08 17:57:09 -0500

    implementación para mostrar la tabla con las personas obtenidas

--- index.js
+++ index.js
@@ -4,30 +4,9 @@ const port = 3000;
 const path = require("path");
 const https = require('https');
 
-/*function obtenerPersonas() {
-  const datos_ejemplo = {
-    url:'https://8j5baasof2.execute-api.us-west-2.amazonaws.com/production/tests/trucode/samples?size=10',
-    method: 'GET',
-    json:{},
-    qs:{
-      offset:20
-    }
-  };
-  request(datos_ejemplo, (err, response, body) => {
-    if (err) {
-      console.log(err);
-    } else if (response.statusCode === 200) {
-      console.log(body);
-    } else {
-      console.log(response.statusCode);
-    }
-  });
-}*/
-
-
 app.get("/",(request, response) => {
   response.sendFile(path.resolve(__dirname, 'index.html'));
-})
+});
 
 app.get("/obtener/:amount",(request, response) => {
   https.get(`https://8j5baasof2.execute-api.us-west-2.amazonaws.com/production/tests/trucode/samples?size=${request.params.amount}`, (res) => {
@@ -40,7 +19,11 @@ app.get("/obtener/:amount",(request, response) => {
       response.send(data.split('\n'));
     });
   });
-})
+});
+
+app.post("/enviar", (request, response) => {
+
+});
 
 app.listen(port,() => {
   console.log("Mi port" + 3000);
